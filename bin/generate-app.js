9c42eb1985a9369b9e764c25b7e474c3df35d630
zivnadel <zywn1414@gmail.com>
2022-07-16 23:00:14 +0300

    .

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,8 +1,6 @@
-#!/usr/bin/env node
-
-const { execSync } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+const { execSync } = require("child_process");
 
 if (process.argv.length < 3) {
 	console.log("You have to provide a name to your app.");
